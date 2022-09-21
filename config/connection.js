8a92f2e77ceac5c5b45be32f225dd1d64cfc5eb5
Rikayla <Rikaylaj@gmail.com>
2022-09-20 20:06:03 -0400

    trying to seed

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(jrghv23tdma7fzvg, oubwz34876531k3j, fursenat1gp4p6cl, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'migae5o25m2psr4q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306
